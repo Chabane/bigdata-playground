86abb1cd3aec3423009bf7dadc4ee261d271cf7d
Chabane <chabane.refes@gmail.com>
2017-12-23 22:47:55 +0100

    feature: read parquets file

--- batch/src/main/scala/com/mitosis/Main.scala
+++ batch/src/main/scala/com/mitosis/Main.scala
@@ -1,8 +1,25 @@
 package com.mitosis
 
+import com.mitosis.config.ConfigurationFactory
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+
 object Main {
 
+  private[this] lazy val logger = Logger.getLogger(getClass)
+
+  private[this] val config = ConfigurationFactory.load()
+
   def main(args: Array[String]): Unit = {
-    
+
+    val sparkSession = SparkSession.builder
+      .appName("search-flight-batch")
+      .getOrCreate()
+
+    val parquetFileDF = sparkSession.read.parquet(args(0))
+
+    parquetFileDF.createOrReplaceTempView("airportParquet")
+    val airportDF = sparkSession.sql("SELECT * FROM airportParquet")
+    airportDF.show()
   }
 }
