e7d792aef609c986a5a826193dc972ff3c05d8f2
Chabane <chabane.refes@gmail.com>
2017-12-24 15:57:43 +0100

    refactor: remove unused files

--- webapp/server/src/kafka/producer.ts
+++ webapp/server/src/kafka/producer.ts
@@ -7,60 +7,68 @@ export class KafkaProducer {
     private topic = 'flightInfoTopic';
 
     constructor() {
-        const client = new Client('zookeeper:2181');
+        const client = new Client('192.168.0.32:2181');
         this.producer = new Producer(client, { requireAcks: 1 });
     }
 
     initialize() {
-        this.producer.on('ready', this.onReady);
+        console.log('Initializing');
+        this.producer.on('ready', () => {
+
+            const schemaType = Type.forSchema({
+                type: 'record',
+                fields: [
+                    { name: 'departing', type: 'string' },
+                    { name: 'arriving', type: 'string' },
+                    { name: 'tripType', type: { type: 'enum', symbols: ['ONE_WAY', 'ROUND_TRIP'] } },
+                    { name: 'departingDate', type: {type: 'long', logicalType: 'timestamp-millis'} },
+                    { name: 'arrivingDate', type: {type: 'long', logicalType: 'timestamp-millis'} },
+                    { name: 'passengerNumber', type: 'int' },
+                    { name: 'cabinClass', type: { type: 'enum', symbols: ['ECONOMY', 'PRENIUM', 'BUSINESS'] } }
+                ]
+            });
+
+            // const buffer = toMessageBuffer({}, schemaType, 1);
+            // const keyedMessage = new KeyedMessage('event', <any>buffer);
+            const buffer = schemaType.toBuffer(<any>{
+                departing: 'now',
+                arriving: 'bidule',
+                tripType: 'ONE_WAY',
+                departingDate: 1514125874,
+                arrivingDate: 1514125874,
+                passengerNumber: 3,
+                cabinClass: 'ECONOMY'
+            });
+
+            const keyedMessage = new KeyedMessage('event', buffer);
+
+            this.producer.send([
+                { topic: this.topic, partition: 1, messages: [keyedMessage] }
+            ], function (err, result) {
+                console.log(err || result);
+                process.exit();
+            });
+        });
         this.producer.on('error', this.onError);
     }
 
-    onReady() {
-
-        console.log('Ready to send!');
-
-        const type = Type.forSchema({
-            type: 'record',
-            fields: [
-                { name: 'departing', type: 'string' },
-                { name: 'arriving', type: 'string' },
-                { name: 'tripType', type: { type: 'enum', symbols: ['ONE_WAY', 'ROUND_TRIP'] } },
-                { name: 'departingDate', type: 'timestamp-millis' },
-                { name: 'arrivingDate', type: 'timestamp-millis' },
-                { name: 'passengerNumber', type: 'int' },
-                { name: 'cabinClass', type: { type: 'enum', symbols: ['ECONOMY', 'PRENIUM', 'BUSINESS'] } },
-            ]
-        });
-
-        const buffer = this.toMessageBuffer({}, type, 1);
-        const keyedMessage = new KeyedMessage('event', <any>buffer);
-
-        const message = 'a message';
-        this.producer.send([
-            { topic: this.topic, partition: 1, messages: [message, keyedMessage] }
-        ], function (err, result) {
-            console.log(err || result);
-            process.exit();
-        });
-    }
-
-    onError(err) {
+    private onError(err) {
         console.log('error', err);
     }
 
+
     /**
-   * Encode an Avro value into a message, as expected by Confluent's Kafka Avro
-   * deserializer.
-   *
-   * @param val {...} The Avro value to encode.
-   * @param type {Type} Your value's Avro type.
-   * @param schemaId {Integer} Your schema's ID (inside the registry).
-   * @param length {Integer} Optional initial buffer length. Set it high enough
-   * to avoid having to resize. Defaults to 1024.
-   *
-   */
-    toMessageBuffer(val: any, type, schemaId, length?) {
+     * Encode an Avro value into a message, as expected by Confluent's Kafka Avro
+     * deserializer.
+     *
+     * @param val {...} The Avro value to encode.
+     * @param type {Type} Your value's Avro type.
+     * @param schemaId {Integer} Your schema's ID (inside the registry).
+     * @param length {Integer} Optional initial buffer length. Set it high enough
+     * to avoid having to resize. Defaults to 1024.
+     *
+     */
+    private toMessageBuffer(val: any, type, schemaId, length?): any {
         const buf = new Buffer(length || 1024);
         buf[0] = 0; // Magic byte.
         buf.writeInt32BE(schemaId, 1);
