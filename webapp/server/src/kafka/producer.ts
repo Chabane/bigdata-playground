2d969207e6766aef52ffef6894dfa2530e9bfc37
Chabane <chabane.refes@gmail.com>
2017-12-31 00:50:54 +0100

    feature(kafka): evolve producer

--- webapp/server/src/kafka/producer.ts
+++ webapp/server/src/kafka/producer.ts
@@ -1,5 +1,6 @@
 import { Producer, KeyedMessage, Client } from 'kafka-node';
-import { parse, Type } from 'avsc/lib';
+import { parse, Type as AvroType} from 'avsc/lib';
+import * as winston from 'winston';
 
 export class KafkaProducer {
 
@@ -7,37 +8,34 @@ export class KafkaProducer {
     private topic = 'flightInfoTopic';
 
     constructor() {
-        const client = new Client('192.168.0.32:2181');
+        const client = new Client('zookeeper-1.vnet:2181');
         this.producer = new Producer(client, { requireAcks: 1 });
     }
 
     sendFlightInfo(flightInfo) {
-        console.log('Initializing');
+        winston.info('Initializing producer');
         this.producer.on('ready', () => {
-
-            const schemaType = Type.forSchema({
+            const schemaType = AvroType.forSchema({
                 type: 'record',
+                name: 'flightInfo',
                 fields: [
                     { name: 'departing', type: 'string' },
                     { name: 'arriving', type: 'string' },
-                    { name: 'tripType', type: { type: 'enum', symbols: ['ONE_WAY', 'ROUND_TRIP'] } },
+                    { name: 'tripType', type: { type: 'enum', name:'TripType', symbols: ['ONE_WAY', 'ROUND_TRIP'] } },
                     { name: 'departingDate', type: {type: 'long', logicalType: 'timestamp-millis'} },
                     { name: 'arrivingDate', type: {type: 'long', logicalType: 'timestamp-millis'} },
                     { name: 'passengerNumber', type: 'int' },
-                    { name: 'cabinClass', type: { type: 'enum', symbols: ['ECONOMY', 'PRENIUM', 'BUSINESS'] } }
+                    { name: 'cabinClass', type: { type: 'enum', name:'CabinClass', symbols: ['ECONOMY', 'PRENIUM', 'BUSINESS'] } }
                 ]
             });
 
-            // const buffer = toMessageBuffer({}, schemaType, 1);
-            // const keyedMessage = new KeyedMessage('event', <any>buffer);
-            const buffer = schemaType.toBuffer(<any>flightInfo);
-
-            const keyedMessage = new KeyedMessage('event', buffer);
+            const buffer = new KeyedMessage('event', <any>flightInfo);
+            const keyedMessage = new KeyedMessage('key', buffer);
 
             this.producer.send([
-                { topic: this.topic, partition: 0, messages: [keyedMessage] }
-            ], function (err, result) {
-                console.log(err || result);
+                { topic: this.topic, partition: 0, messages: keyedMessage }
+            ], (err, result) => {
+                winston.error(err || result);
                 process.exit();
             });
         });
@@ -45,6 +43,6 @@ export class KafkaProducer {
     }
 
     private onError(err) {
-        console.log('error', err);
+        winston.error('error', err);
     }
 }
