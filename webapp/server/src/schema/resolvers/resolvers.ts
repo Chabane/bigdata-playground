6ec2eb3d21e9f71f02854c10af1b53334d93b358
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2017-12-31 16:28:06 +0100

    feature: add fetchAirports to Query resolvers

--- webapp/server/src/schema/resolvers/resolvers.ts
+++ webapp/server/src/schema/resolvers/resolvers.ts
@@ -1,13 +1,18 @@
 import { IFlightInfo } from '../../db';
 import { KafkaProducer } from '../../kafka';
+import { Airport } from '../../db/models/airport';
 
 const producer = new KafkaProducer();
 
 export const resolvers = {
-    Query: {
-      sendFlightInfo: async (_, { fightInfo }) => {
-        producer.sendFlightInfo(fightInfo as IFlightInfo);
-        return fightInfo;
-      }
+  Query: {
+    sendFlightInfo: async (_, { fightInfo }) => {
+      producer.sendFlightInfo(fightInfo as IFlightInfo);
+      return fightInfo;
+    },
+    fetchAirports: async () => {
+      const airports = await Airport.findAirports();
+      return airports;
+    }
   }
 };
