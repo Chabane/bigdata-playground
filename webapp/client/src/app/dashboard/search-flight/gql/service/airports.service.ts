31d7fd24a3d4f799ce183456117b7dc1995c0b09
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2018-01-14 18:45:33 +0100

    feature: add getDestinationAirports service

--- webapp/client/src/app/dashboard/search-flight/gql/service/airports.service.ts
+++ webapp/client/src/app/dashboard/search-flight/gql/service/airports.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 
-import { AIRPORTS_QUERY } from '../query/airports.queries';
+import { AIRPORTS_QUERY, DESTINATION_AIRPORTS_QUERY } from '../query/airports.queries';
 
 
 @Injectable()
@@ -16,6 +16,17 @@ export class AirportsService {
             variables: {
                 departingFrom: departingFrom
             }
+        });
+    }
+
+    getDestinationAirports(departingAirportId: string, arrivingAt: string) {
+        return this.apollo.query({
+            query: gql`${DESTINATION_AIRPORTS_QUERY}`,
+            variables: {
+                departingAirportId: departingAirportId,
+                arrivingAt: arrivingAt
+            },
+            fetchPolicy: 'network-only'
         });
     }
 }
