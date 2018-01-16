995456b0f8225b9fa9c6e1b69958a7117771b045
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2018-01-16 20:46:20 +0100

    feature: replace two queries with fetchAirports

--- webapp/client/src/app/dashboard/search-flight/gql/query/airports.queries.ts
+++ webapp/client/src/app/dashboard/search-flight/gql/query/airports.queries.ts
@@ -1,18 +1,6 @@
 export const AIRPORTS_QUERY =
-    `query airports($departingFrom:String) {
-    fetchAirports(departingFrom:$departingFrom) {
-        AirportID
-        City
-        Country
-        Name
-        destinations
-    }
-  }
-`;
-
-export const DESTINATION_AIRPORTS_QUERY =
-    `query destinationAirports($departingAirportId:String, $arrivingAt:String) {
-    fetchDestinationAirports(departingAirportId:$departingAirportId, arrivingAt:$arrivingAt) {
+    `query airports($airportToSearch:String, $airportId:String) {
+        fetchAirports(airportToSearch:$airportToSearch, airportId:$airportId) {
         AirportID
         City
         Country
