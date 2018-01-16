c5c5ba29f99018882983ac8bd8ae21b3ab602a11
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2018-01-16 20:45:53 +0100

    feature: replace two services with getAirports

--- webapp/client/src/app/dashboard/search-flight/gql/service/airports.service.ts
+++ webapp/client/src/app/dashboard/search-flight/gql/service/airports.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 
-import { AIRPORTS_QUERY, DESTINATION_AIRPORTS_QUERY } from '../query/airports.queries';
+import { AIRPORTS_QUERY } from '../query/airports.queries';
 
 
 @Injectable()
@@ -10,21 +10,12 @@ export class AirportsService {
     constructor(private apollo: Apollo) {
     }
 
-    getAirports(departingFrom: string) {
+    getAirports(airportToSearch: string, airportId?: string) {
         return this.apollo.query({
             query: gql`${AIRPORTS_QUERY}`,
             variables: {
-                departingFrom: departingFrom
-            }
-        });
-    }
-
-    getDestinationAirports(departingAirportId: string, arrivingAt: string) {
-        return this.apollo.query({
-            query: gql`${DESTINATION_AIRPORTS_QUERY}`,
-            variables: {
-                departingAirportId: departingAirportId,
-                arrivingAt: arrivingAt
+                airportId: airportId,
+                arrivingAt: airportToSearch
             },
             fetchPolicy: 'network-only'
         });
