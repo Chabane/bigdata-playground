f03e6f325caa8b6441968551797bfd3126c9d5c6
Chabane <chabane.refes@gmail.com>
2018-03-03 23:14:28 +0100

    bugfix: fix database name

--- webapp/server/src/db/config/database.ts
+++ webapp/server/src/db/config/database.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 import * as winston from 'winston';
 
-mongoose.connect('mongodb://mongo.vnet:27017/flight');
+mongoose.connect('mongodb://mongo.vnet:27017/mitosis');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
