a9079a510bdafc6889fbb2e65be1090c38e6cfb2
Chabane <chabane.refes@gmail.com>
2018-03-04 15:37:51 +0100

    feature: initial commit

---
+++ webapp/server/src/kafka/flight-info-avro.mapper.ts
@@ -0,0 +1,27 @@
+import { IFlightInfo } from "../db";
+import { FlightInfoAvro } from './flight-info-avro';
+
+export class FlightInfoAvroMapper {
+
+    public static toFlightInfoAvro(flightInfo: IFlightInfo): FlightInfoAvro {
+        let flightInfoAvro: FlightInfoAvro; 
+        if(flightInfo) {
+            console.log(flightInfo);
+            flightInfoAvro = new FlightInfoAvro();
+            flightInfoAvro.arrivalDate = flightInfo.arrivalDate? 
+                new Date(flightInfo.arrivalDate).getTime(): undefined;
+            flightInfoAvro.arrivingId = flightInfo.arrivingId;
+            flightInfoAvro.cabinClass = flightInfo.cabinClass;
+            flightInfoAvro.departingId = flightInfo.departingId;
+            flightInfoAvro.departureDate = flightInfo.departureDate? 
+                new Date(flightInfo.departureDate).getTime(): undefined;
+            flightInfoAvro.ipAddress = undefined;
+            flightInfoAvro.latitude = undefined;
+            flightInfoAvro.longitude = undefined;
+            flightInfoAvro.passengerNumber = flightInfo.passengerNumber;
+            flightInfoAvro.tripType = flightInfo.tripType
+            flightInfoAvro.eventTime = new Date().getTime();
+        }
+        return flightInfoAvro;
+    } 
+}
---
+++ webapp/server/src/kafka/flight-info-avro.ts
@@ -0,0 +1,14 @@
+
+export class FlightInfoAvro {
+  departingId: string;
+  ipAddress: string;
+  latitude: string;
+  longitude: string;
+  arrivingId: string;
+  tripType: string;
+  departureDate: number;
+  arrivalDate: number;
+  passengerNumber: number;
+  cabinClass: string;
+  eventTime: number;
+}
