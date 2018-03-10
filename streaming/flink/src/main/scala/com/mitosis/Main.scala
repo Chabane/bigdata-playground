23041334c2746b5b2f8868382f51fa2f9712078b
Chabane <chabane.refes@gmail.com>
2018-03-10 21:17:11 +0100

    feature: reading avro info

--- streaming/flink/src/main/scala/com/mitosis/Main.scala
+++ streaming/flink/src/main/scala/com/mitosis/Main.scala
@@ -8,30 +8,59 @@ import com.mitosis.utils.JsonUtils
 import com.mitosis.config.ConfigurationFactory
 import org.apache.log4j.Logger
 
+import org.apache.avro.io.DatumReader
+import org.apache.avro.io.Decoder
+import org.apache.avro.specific.SpecificDatumReader
+import org.apache.avro.generic.GenericRecord
+import org.apache.avro.io.DecoderFactory
+import org.apache.avro.SchemaBuilder
+
+import scala.io.Source
+
+import org.apache.avro.Schema
+import org.apache.avro.Schema.Parser
+
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.util.serialization.DeserializationSchema
+
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
 
+import scala.io.Source
+
 object Main {
 
-  private[this] lazy val logger = Logger.getLogger(getClass)
+ private[this] lazy val logger = Logger.getLogger(getClass)
 
-  private[this] val config = ConfigurationFactory.load()
+ private[this] val config = ConfigurationFactory.load()
 
-  /**
-    * Json decode UDF function
-    *
-    * @param text the encoded JSON string
-    * @return Returns record bean
-    */
+
+ val flightInfoHbaseSchema = s"""{
+                |"table":{"namespace":"default", "name":"flightInfo", "tableCoder":"PrimitiveType"},
+                |"rowkey":"key",
+                |"columns":{
+                |"key":{"cf":"rowkey", "col":"key", "type":"string"},
+                |"departingId":{"cf":"searchFlightInfo", "col":"departingId", "type":"string"},
+                |"arrivingId":{"cf":"searchFlightInfo", "col":"arrivingId", "type":"string"},
+                |"tripType":{"cf":"searchFlightInfo", "col":"tripType", "type":"string"},
+                |"departureDate":{"cf":"searchFlightInfo", "col":"departureDate", "type":"bigint"},
+                |"arrivalDate":{"cf":"searchFlightInfo", "col":"arrivalDate", "type":"bigint"},
+                |"passengerNumber":{"cf":"searchFlightInfo", "col":"passengerNumber", "type":"integer"},
+                |"cabinClass":{"cf":"searchFlightInfo", "col":"cabinClass", "type":"string"}
+                |}
+                |}""".stripMargin
+
   def jsonDecode(text: String): FlightInfoBean = {
     try {
-      JsonUtils.deserialize(text, classOf[FlightInfoBean])
+    JsonUtils.deserialize(text, classOf[FlightInfoBean])
     } catch {
-      case e: IOException =>
-        logger.error(e.getMessage, e)
-        null
+    case e:
+      IOException =>
+      logger.error(e.getMessage, e)
+      null
     }
   }
 
@@ -49,13 +78,35 @@ object Main {
     properties.setProperty("bootstrap.servers", servers)
     properties.setProperty("group.id", "mitosis")
 
-    val kafkaConsumer = new FlinkKafkaConsumer011(topic, new SimpleStringSchema, properties)
+    val kafkaConsumer = new FlinkKafkaConsumer011(topic, FlightInfoAvroSchema, properties)
     kafkaConsumer.setStartFromLatest()
 
     val messageStream = env.addSource(kafkaConsumer)
-    messageStream.print()
+    messageStream.map(record => {
+      println(jsonDecode(record))
+      record
+    })
 
+    messageStream.print()
     env.execute("search-flight-flink-streaming")
   }
 
+  object FlightInfoAvroSchema extends DeserializationSchema[String] {
+
+    import org.apache.flink.api.common.typeinfo.TypeInformation
+    import org.apache.flink.api.java.typeutils.TypeExtractor
+
+    override def isEndOfStream(t: String): Boolean = false
+
+    override def deserialize(message: Array[Byte]): String = {
+      val flightInfoAvroSchema: Schema = new Parser().parse(Source.fromURL(getClass.getResource("/flight-info.schema.avsc")).mkString)
+
+      val reader: DatumReader[GenericRecord] = new SpecificDatumReader[GenericRecord](flightInfoAvroSchema)
+      val decoder: Decoder = DecoderFactory.get().binaryDecoder(message, null)
+
+      val flightInfoJson: GenericRecord = reader.read(null, decoder)
+      flightInfoJson.toString
+    }
+    override def getProducedType: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
+  }
 }
