ab6a5e262eb14240f865c0bb0c3b26f4b41af24a
Chabane <chabane.refes@gmail.com>
2018-03-11 17:54:13 +0100

    feature: save date into base

--- streaming/flink/src/main/scala/com/mitosis/Main.scala
+++ streaming/flink/src/main/scala/com/mitosis/Main.scala
@@ -1,33 +1,17 @@
 package com.mitosis
 
-import java.io.IOException
 import java.util.Properties
 
 import com.mitosis.beans.FlightInfoBean
-import com.mitosis.utils.JsonUtils
+import com.mitosis.utils.FlightInfoAvroSchema
+import com.mitosis.utils.FlightInfoHBaseOutputFormat
 import com.mitosis.config.ConfigurationFactory
 import org.apache.log4j.Logger
 
-import org.apache.avro.io.DatumReader
-import org.apache.avro.io.Decoder
-import org.apache.avro.specific.SpecificDatumReader
-import org.apache.avro.generic.GenericRecord
-import org.apache.avro.io.DecoderFactory
-import org.apache.avro.SchemaBuilder
-
-import scala.io.Source
-
-import org.apache.avro.Schema
-import org.apache.avro.Schema.Parser
-
-import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.util.serialization.DeserializationSchema
-
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+import org.apache.flink.streaming.api.scala._
 
 import scala.io.Source
 
@@ -36,33 +20,6 @@ object Main {
  private[this] lazy val logger = Logger.getLogger(getClass)
 
  private[this] val config = ConfigurationFactory.load()
-
-
- val flightInfoHbaseSchema = s"""{
-                |"table":{"namespace":"default", "name":"flightInfo", "tableCoder":"PrimitiveType"},
-                |"rowkey":"key",
-                |"columns":{
-                |"key":{"cf":"rowkey", "col":"key", "type":"string"},
-                |"departingId":{"cf":"searchFlightInfo", "col":"departingId", "type":"string"},
-                |"arrivingId":{"cf":"searchFlightInfo", "col":"arrivingId", "type":"string"},
-                |"tripType":{"cf":"searchFlightInfo", "col":"tripType", "type":"string"},
-                |"departureDate":{"cf":"searchFlightInfo", "col":"departureDate", "type":"bigint"},
-                |"arrivalDate":{"cf":"searchFlightInfo", "col":"arrivalDate", "type":"bigint"},
-                |"passengerNumber":{"cf":"searchFlightInfo", "col":"passengerNumber", "type":"integer"},
-                |"cabinClass":{"cf":"searchFlightInfo", "col":"cabinClass", "type":"string"}
-                |}
-                |}""".stripMargin
-
-  def jsonDecode(text: String): FlightInfoBean = {
-    try {
-    JsonUtils.deserialize(text, classOf[FlightInfoBean])
-    } catch {
-    case e:
-      IOException =>
-      logger.error(e.getMessage, e)
-      null
-    }
-  }
 
   def main(args: Array[String]): Unit = {
 
@@ -82,31 +39,10 @@ object Main {
     kafkaConsumer.setStartFromLatest()
 
     val messageStream = env.addSource(kafkaConsumer)
-    messageStream.map(record => {
-      println(jsonDecode(record))
-      record
-    })
+    messageStream.writeUsingOutputFormat(new FlightInfoHBaseOutputFormat)
 
-    messageStream.print()
     env.execute("search-flight-flink-streaming")
   }
 
-  object FlightInfoAvroSchema extends DeserializationSchema[String] {
 
-    import org.apache.flink.api.common.typeinfo.TypeInformation
-    import org.apache.flink.api.java.typeutils.TypeExtractor
-
-    override def isEndOfStream(t: String): Boolean = false
-
-    override def deserialize(message: Array[Byte]): String = {
-      val flightInfoAvroSchema: Schema = new Parser().parse(Source.fromURL(getClass.getResource("/flight-info.schema.avsc")).mkString)
-
-      val reader: DatumReader[GenericRecord] = new SpecificDatumReader[GenericRecord](flightInfoAvroSchema)
-      val decoder: Decoder = DecoderFactory.get().binaryDecoder(message, null)
-
-      val flightInfoJson: GenericRecord = reader.read(null, decoder)
-      flightInfoJson.toString
-    }
-    override def getProducedType: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
-  }
 }
---
+++ streaming/flink/src/main/scala/com/mitosis/utils/FlightInfoHBaseOutputFormat.scala
@@ -0,0 +1,78 @@
+package com.mitosis.utils
+
+import org.apache.hadoop.hbase.HBaseConfiguration
+
+import org.apache.flink.api.common.io.OutputFormat
+import org.apache.flink.configuration.Configuration
+
+import org.apache.hadoop.hbase.{ HColumnDescriptor, HTableDescriptor, HBaseConfiguration } 
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.hbase.client.HTable
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.hbase.util.Bytes
+import com.mitosis.beans.FlightInfoBean
+import java.io.IOException
+import org.apache.log4j.Logger
+
+  /**
+	 * This class implements an OutputFormat for HBase.
+	 */
+	class FlightInfoHBaseOutputFormat extends OutputFormat[String] {
+
+ private[this] lazy val logger = Logger.getLogger(getClass)
+
+  def jsonDecode(text: String): FlightInfoBean = {
+    try {
+      JsonUtils.deserialize(text, classOf[FlightInfoBean])
+    } catch {
+    case e:
+      IOException =>
+      logger.error(e.getMessage, e)
+      null
+    }
+  }
+
+		private var conf: org.apache.hadoop.conf.Configuration = null
+    private var flightInfoTable: HTable = null
+
+    override def configure(parameters: Configuration): Unit = {
+      conf = HBaseConfiguration.create()
+      conf.addResource("hbase-site.xml")
+
+      val admin = new HBaseAdmin(conf)
+
+       if (!admin.isTableAvailable("flightInfo")) {
+          val flightInfoTableDesc = new HTableDescriptor(Bytes.toBytes("flightInfo"))
+          val searchFlightInfoColumnFamilyDesc = new HColumnDescriptor(Bytes.toBytes("searchFlightInfo"))
+          flightInfoTableDesc.addFamily(searchFlightInfoColumnFamilyDesc)
+          admin.createTable(flightInfoTableDesc)
+        }
+    }
+
+    override def open(taskNumber: Int, numTasks: Int): Unit = {
+      flightInfoTable = new HTable(conf, "flightInfo")
+    }
+
+    override def writeRecord(record: String): Unit = {
+      val flightInfo = jsonDecode(record)
+      val random = scala.util.Random
+      val rowKey = random.nextLong
+      val flightInfoRow = new Put(Bytes.toBytes(rowKey))
+
+      flightInfoRow.add(Bytes.toBytes("searchFlightInfo"),Bytes.toBytes("departingId"),Bytes.toBytes(flightInfo.departingId))
+      flightInfoRow.add(Bytes.toBytes("searchFlightInfo"),Bytes.toBytes("arrivingId"),Bytes.toBytes(flightInfo.arrivingId))
+      flightInfoRow.add(Bytes.toBytes("searchFlightInfo"),Bytes.toBytes("tripType"),Bytes.toBytes(flightInfo.tripType))
+      flightInfoRow.add(Bytes.toBytes("searchFlightInfo"),Bytes.toBytes("departureDate"),Bytes.toBytes(flightInfo.departureDate))
+      flightInfoRow.add(Bytes.toBytes("searchFlightInfo"),Bytes.toBytes("arrivalDate"),Bytes.toBytes(flightInfo.arrivalDate))
+      flightInfoRow.add(Bytes.toBytes("searchFlightInfo"),Bytes.toBytes("passengerNumber"),Bytes.toBytes(flightInfo.passengerNumber))
+      flightInfoRow.add(Bytes.toBytes("searchFlightInfo"),Bytes.toBytes("cabinClass"),Bytes.toBytes(flightInfo.cabinClass))
+
+      flightInfoTable.put(flightInfoRow)
+    }
+
+    override def close(): Unit = {
+      flightInfoTable.flushCommits()
+      flightInfoTable.close()
+    }
+	}
