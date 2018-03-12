fd181f092570b1a174c436aa5e7a0236322d563f
Chabane <chabane.refes@gmail.com>
2018-03-12 21:49:53 +0100

    docs: MongoDB Connector not implemented yet

--- batch/flink/src/main/scala/com/mitosis/Main.scala
+++ batch/flink/src/main/scala/com/mitosis/Main.scala
@@ -14,5 +14,6 @@ object Main {
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
     // ParquetInputFormat not implemented yet - FLINK-7243 - https://issues.apache.org/jira/browse/FLINK-7243
+    // MongoDB Connector not implemented yet - FLINK-6573 - https://issues.apache.org/jira/browse/FLINK-6573
   }
 }
