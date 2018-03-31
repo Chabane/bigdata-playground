d6c5488a54b032531680f18e4a8986695e1a0fbd
Chabane <chabane.refes@gmail.com>
2018-03-31 22:16:42 +0200

    bugfix: read value of message

--- webapp/server/src/kafka/tweet.consumer.ts
+++ webapp/server/src/kafka/tweet.consumer.ts
@@ -1,5 +1,5 @@
 import { Consumer, KeyedMessage, KafkaClient, Client } from 'kafka-node';
-import { parse, Type as AvroType } from 'avsc/lib';
+import { Type as AvroType } from 'avsc/lib';
 import * as winston from 'winston';
 import { ITweet } from '../db';
 import { pubsub } from '../schema';
@@ -8,29 +8,28 @@ export class KafkaTweetConsumer {
 
     private client: KafkaClient;
     private consumer: Consumer;
-    private topic = 'flightInfoTopic';
+    private topic = 'tweetsTopic';
 
     constructor() {
         this.client = new KafkaClient({ kafkaHost: 'kafka.vnet:9092' });
         this.client.on('connect', () => {
-            winston.info('Connected to Kafka');
+            winston.info('Tweet Kafka Client connected to Kafka');
         });
         this.client.on('error', (error) => {
-            winston.error('Kafka - Client error > ', error);
+            winston.error('Tweet Kafka Client - error > ', error);
         });
 
         this.consumer = new Consumer(this.client,
         [
-            { topic: 'tweetsTopic', partition: 0 }
+            { topic: this.topic, partition: 0 }
         ],
         {
-            autoCommit: false,
-            fetchMaxWaitMs: 1000,
-            fetchMaxBytes: 1024 * 1024
+            autoCommit: false, 
+            encoding: 'buffer'
         });
 
         this.consumer.on('error', (error) => {
-            winston.error('Kafka - consumer error > ', error);
+            winston.error('Tweet Kafka Consumer - error > ', error);
         });
         this.consumer.on('message', (message: any) => {
             this.consumeTweets(message);
@@ -46,8 +45,7 @@ export class KafkaTweetConsumer {
                 { name: 'id', type: 'string' }
             ]
         });
-
-        tweets = schemaType.fromBuffer(message);
+        tweets = schemaType.fromBuffer(message.value);
         pubsub.publish('tweets', { data: tweets });
     }
 }
