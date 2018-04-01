0c0ce16fda61bbc1445a5095384c5764d4df2ad9
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2018-04-01 21:41:09 +0200

    feature: change host from mongo.vnet to mongo

--- webapp/server/src/db/config/database.ts
+++ webapp/server/src/db/config/database.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 import * as winston from 'winston';
 
-mongoose.connect('mongodb://mongo.vnet:27017/mitosis');
+mongoose.connect('mongodb://mongo:27017/mitosis');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
